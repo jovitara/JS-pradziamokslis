f07bca8f908abe2bb322765c7a79eb53691e68f7
jovitara <83599193+jovitara@users.noreply.github.com>
2021-07-05 19:19:42 +0300

    submit klaida

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,39 +14,192 @@ skaiciavimas = skaiciavimas / 3;
 
 //alert(skaiciavimas);
 
-var petras = [0, "Labas lietuva", 2, 3, [1, 2, 3]];
+var petras = [
+    0 , 
+    15,
+    2 , 
+    3 , 
+];
 
 //console.log( petras[1] );
 
-//var = variable (angl.) kintamasis
+//var = variable (angl.) kintamasis 
 
 //Eventas skirtas issaukti reakcija paspaudus mygtuka
-var pasisveikinimas = "Labas lietuva!";
+var pasisveikinimas = 'Labas lietuva!';
 
-document.getElementById("kvietejas").addEventListener("click", ikrepseli);
+
+
+
+
+
+
+document.getElementById('kvietejas').addEventListener("click", ikrepseli);
 
 function ikrepseli() {
-  var kiekis = document.getElementById("kiekis").value;
-  var zodis = "prekes";
-  var zinute = "Jūs sėkmingai pridėjote " + kiekis + " prekę į savo krepšelį!";
 
-  if (kiekis % 10 == 1) {
-    zodis = "prekę";
-  }
+    var kiekis = document.getElementById('kiekis').value;
+    var zodis  = 'prekes';
 
-  if ((kiekis > 10 && kiekis < 20) || kiekis % 10 == 0) {
-    zodis = "prekių";
-  }
+    if(kiekis%10 == 1) {
+        zodis  = 'prekę';
+    }
 
-  if (kiekis < 1) {
-    zinute = "Pasirinktas per mažas kiekis.";
+    if( (kiekis > 10 && kiekis < 20) || kiekis%10 == 0) {
+        zodis = 'prekių';
+    } 
+
+    var zinute = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
+
+    if(kiekis > 100) 
+         zinute = 'Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas.'
+
+    if(kiekis < 1)
+         zinute = 'Pasirinktas per mažas kiekis.';
+
+    document.getElementById('messages').innerHTML = '';
+
+    //alert('Jūs sėkmingai pridėjote ' + kiekis + ' prekę į savo krepšelį!');
+}
+
+//kaunasLietuva(petras);
+
+
+document.getElementById('reset').addEventListener("click", atnaujinti);
+
+function atnaujinti() {
+
+    location.reload();
+
+}
+
+var i = 0;
+
+while(i < 10) {
+
+    //console.log(i);
+    i++;
+
+}
+
+i = 0;
+
+do {
+    //console.log('Skaicius yra: ' + i);
+    i++;
+}
+while (i < 10)
+
+
+var petras = [
+    5 , 
+    6, 
+    7 , 
+    8 , 
+    9
+];
+
+//console.log(petras[0]);
+
+/*for(let i = 0; i < petras.length; i++) {
+    console.log('Masyvo petras indeksas: ' + petras[i]);
+}*/
+
+const duomenys = [
+    {
+        miestas: "Kaunas",
+        adresas: "Jonavos 56",
+        data: "2020-07-01",
+        kiekis: "10"
+    },
+    {
+        miestas: "Vilnius",
+        adresas: "Gelezinio vilko 20",
+        data: "2020-07-01",
+        kiekis: "15"
+    },
+    {
+        miestas: "Klaipeda",
+        adresas: "Ligonines 20",
+        data: "2020-07-01",
+        kiekis: "25"
+    }
+];
+
+
+let html = '';
+let ending = '';
+
+for(let indeksas in duomenys) {
+
+    html += '<tr>'; 
+
+    for(let indeksas2 in duomenys[indeksas]) {
+        
+        html += '<td>' + duomenys[indeksas][indeksas2] + '</td>';
+
+    }
+
+    html += '</tr>';
+
+    //console.log(eilute);
+
+}
+
+document.getElementById('lentelesVidus').innerHTML = html;
+
+function testineFunkcija() {
+
+}
+
+let reiksme = 0;
+
+switch(reiksme) {
+
+    case 0:
+        testineFunkcija();
+    break;
+
+    case 1:
+        alert('Reiksme yra 1');
+    break;
+
+    default:
+        alert('Reiksmes nera');
+
+}
+
+document.getElementById('rodyti').onclick = function() {
+
+    var lentele = document.getElementById('lentele');
     
-  }
-  if (kiekis > 100) {
-    zinute = "Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas.";
-   
-  }
-  document.getElementById("messages").innerHTML = zinute;
+    lentele.classList.toggle('show'); 
 
-  //kaunasLietuva(petras);
+    if( lentele.classList.contains('show') == false ) {
+        document.getElementById('rodyti').innerText = 'Rodyti';
+    } else {
+        document.getElementById('rodyti').innerText = 'Slėpti';
+    }
+
+}
+
+document.getElementById('skaiciuoti').onclick = function() {
+
+    var skaiciuokles = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    ];
+    
+    var total = 0;
+    
+    for(let i = 0; i < skaiciuokles.length; i++) {
+        total += skaiciuokles[i];
+    }
+
+    document.getElementById('total').innerHTML = total;
+
+}
+
+document.getElementById('submit').onclick = function() {
+
+    console.log('cars');
 }
